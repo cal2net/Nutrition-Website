f7245a5eb23a3c14a518a4d9710d1087116636a1
Raidlucky <68414136+Raidlucky@users.noreply.github.com>
2022-09-19 21:33:28 -0600

    slideshow

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 console.log("ready")
 
-function prev_slide() {
-let slides = []
+function next_slide() {
 var slide = document.getElementsByClassName("plan-link")
+let visible = 3 // visible slides
+let invisible = slide.length - visible 
 console.log("slides:", slide.length)
-for (i=0; i < slide.length-1;) {
+for (i=0; i < slide.length;) {
   slide[i].style.display = "none"
   i++
-  console.log(i)
 }
 for (i=0; i < slide.length;) {
+  console.log("added:", i + invisible)
+  slide[i + invisible].style.display = "inline-block"
   i++
-  slide[i].style.display = "inline-block"
+  
 }
 }
 
-function next_slide() {
-
+function prev_slide() {
+  var slide = document.getElementsByClassName("plan-link")
+  let visible = 3 // visible slides
+  let invisible = slide.length - visible 
+  console.log("slides:", slide.length)
+  for (i=0; i < slide.length;) {
+    slide[i].style.display = "none"
+    i++
+  }
+  for (i=0; i < slide.length - invisible;) {
+    console.log("added:", i)
+    slide[i].style.display = "inline-block"
+    i++
+  }
 }
