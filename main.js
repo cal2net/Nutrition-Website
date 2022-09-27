6ce90731ca5610926afb3a55d3d25e85750c69a7
Raidlucky <68414136+Raidlucky@users.noreply.github.com>
2022-09-26 21:50:33 -0600

    finishing up templates

--- main.js
+++ main.js
@@ -32,3 +32,29 @@ function prev_slide() {
     i++
   }
 }
+
+//search function
+
+function myFunction() {
+  // Declare variables
+  console.log('searching')
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('a');
+  headers = ul.getElementsByClassName('side-header')
+  console.log(li.length)
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i];
+    txtValue = a.textContent || a.innerText;
+    console.log(a.innerHTML, "innerhtml")
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
