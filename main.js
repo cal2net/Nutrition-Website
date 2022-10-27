5c55c5a1f1bfee66731d0edbbbaf2ae5f6891c10
jackgimre <66290453+jackgimre@users.noreply.github.com>
2022-10-27 18:48:59 -0500

    lbs to protein works!

--- main.js
+++ main.js
@@ -89,4 +89,19 @@ function exit() {
 
 // Hey Jack I'm gonna leave some of the diet equations right here
 // 2.2(lbs) * 0.8 (this is for how much protein they should be getting, for every 2.2 lbs they should get 0.8 grams)
+function lbsToProtein(lbs) {
+  let kgs = lbs * 2.2;
+  let proteinGrams = kgs * 0.8;
+  console.log(proteinGrams);
+  return proteinGrams;
+}
+function submit() {
+  let lbs = parseFloat(document.getElementById('lbs-input').value);
+  let height = document.getElementById('height-input');
+  let fat = document.getElementById('fat-input');
+
+  let proteinGrams = lbsToProtein(lbs);
+}
+
+
 // I'll add more later
